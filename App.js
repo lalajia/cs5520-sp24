478b8756269cd2124bde4d643529bd14589f8b72
xinyue <selenazheng3@gmail.com>
2024-01-09 15:01:12 -0800

    change for footer and header

--- App.js
+++ App.js
@@ -1,11 +1,20 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+
+import React from "react";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.header}>
+        <Text style={styles.headerText}>Header</Text>
+      </View>
+      <View style={styles.content}>
+        <Text>Hello, World!</Text>
+      </View>
+      <View style={styles.footer}>
+        <Text style={styles.footerText}>Footer</Text>
+      </View>
     </View>
   );
 }
@@ -13,8 +22,32 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "white",
+  },
+  header: {
+    height: 150,
+    backgroundColor: "blue",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  headerText: {
+    color: "white",
+    fontSize: 20,
+  },
+  content: {
+    flex: 1,
+    backgroundColor: "white",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  footer: {
+    height: 150,
+    backgroundColor: "blue",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  footerText: {
+    color: "white",
+    fontSize: 20,
   },
 });
