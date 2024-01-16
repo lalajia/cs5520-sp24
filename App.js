11a03f76b9d1db1de6df30cb9f136a97a67463ae
xinyue <selenazheng3@gmail.com>
2024-01-16 14:56:34 -0800

    refactoring code to input.js

--- App.js
+++ App.js
@@ -4,25 +4,17 @@ import { StyleSheet, Text, TextInput, View } from "react-native";
 import React from "react";
 import Header from "./components/Header";
 import { useState } from "react";
+import Input from "./components/Input";
 
 export default function App() {
   const appName = "My second App";
-  function changeTextHandler(changeText) {
-    setText(changeText);
-  }
-  const [text, setText] = useState("");
 
   return (
     <View style={styles.container}>
       <View style={styles.content}>
         <Header name={appName} version={2}></Header>
-        <TextInput
-          placeholder="Input here"
-          style={styles.input}
-          value={text}
-          onChangeText={changeTextHandler}
-        ></TextInput>
-        <Text>{text}</Text>
+        <Input></Input>
+        {/* <Text>{text}</Text> */}
       </View>
     </View>
   );
@@ -33,9 +25,7 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: "white",
   },
-  input: {
-    borderBottomWidth: 2,
-  },
+
   // header: {
   //   height: 150,
   //   backgroundColor: "blue",
--- components/Input.js
+++ components/Input.js
@@ -1,12 +1,27 @@
-import { StyleSheet, Text, View } from "react-native";
-import React from "react";
+import { Button, StyleSheet, Text, TextInput, View } from "react-native";
+import React, { useState } from "react";
 
 export default function Input() {
+  function changeTextHandler(changeText) {
+    setText(changeText);
+  }
+  const [text, setText] = useState("");
   return (
     <View>
-      <Text>Input</Text>
+      <TextInput
+        placeholder="Input here"
+        style={styles.input}
+        value={text}
+        onChangeText={changeTextHandler}
+      ></TextInput>
+      {/* <Button></Button> */}
     </View>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  input: {
+    borderBottomWidth: 2,
+    width: 200,
+  },
+});
