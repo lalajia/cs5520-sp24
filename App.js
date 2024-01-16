28035700ab1ba3c3dd80d6bd3c6676b5b5515033
xinyue <selenazheng3@gmail.com>
2024-01-16 14:51:32 -0800

    textinput

--- App.js
+++ App.js
@@ -1,15 +1,28 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, TextInput, View } from "react-native";
 
 import React from "react";
 import Header from "./components/Header";
+import { useState } from "react";
 
 export default function App() {
-  const appName = "My first App";
+  const appName = "My second App";
+  function changeTextHandler(changeText) {
+    setText(changeText);
+  }
+  const [text, setText] = useState("");
+
   return (
     <View style={styles.container}>
       <View style={styles.content}>
         <Header name={appName} version={2}></Header>
+        <TextInput
+          placeholder="Input here"
+          style={styles.input}
+          value={text}
+          onChangeText={changeTextHandler}
+        ></TextInput>
+        <Text>{text}</Text>
       </View>
     </View>
   );
@@ -20,30 +33,33 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: "white",
   },
-  header: {
-    height: 150,
-    backgroundColor: "blue",
-    alignItems: "center",
-    justifyContent: "center",
+  input: {
+    borderBottomWidth: 2,
   },
-  headerText: {
-    color: "white",
-    fontSize: 20,
-  },
+  // header: {
+  //   height: 150,
+  //   backgroundColor: "blue",
+  //   alignItems: "center",
+  //   justifyContent: "center",
+  // },
+  // headerText: {
+  //   color: "white",
+  //   fontSize: 20,
+  // },
   content: {
     flex: 1,
     backgroundColor: "white",
     alignItems: "center",
     justifyContent: "center",
   },
-  footer: {
-    height: 150,
-    backgroundColor: "blue",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  footerText: {
-    color: "white",
-    fontSize: 20,
-  },
+  // footer: {
+  //   height: 150,
+  //   backgroundColor: "blue",
+  //   alignItems: "center",
+  //   justifyContent: "center",
+  // },
+  // footerText: {
+  //   color: "white",
+  //   fontSize: 20,
+  // },
 });
